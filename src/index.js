8e4072430054fc31a82995af6b6c0cb1a11936fc
KyleOrr676 <Kyle.Orr@hotmail.com>
2021-11-23 13:07:41 +0000

    Working search function

--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import "./index.css";
 class FilmItem extends React.Component {
   render() {
     const film = this.props.film;
-    // const title = film.title;
-    // const filmID = film.filmID;
-    // const length = film.length;
-    // const languageID = film.languageID;
-    // const description = film.description;
     return (
       <tr>
         <td>{film.title}</td>
@@ -78,12 +73,14 @@ class FilmEntry extends React.Component {
     // .then(data => this.setState({ postTitle: data.film_id }));
   }
 
-
-  handleChangeTitle(event) {
-    this.setState({ AddTitle: event.target.value });
-  }
   handleChangeFilmID(event) {
     this.setState({ AddFilmID: event.target.value });
+  }
+  handleChangeLanguageID(event) {
+    this.setState({ AddLanguageID: event.target.value });
+  }
+  handleChangeTitle(event) {
+    this.setState({ AddTitle: event.target.value });
   }
   handleChangeDescription(event) {
     this.setState({ AddDescription: event.target.value });
@@ -91,10 +88,7 @@ class FilmEntry extends React.Component {
   handleChangeLength(event) {
     this.setState({ AddLength: event.target.value });
   }
-  handleChangeLanguageID(event) {
-    this.setState({ AddLanguageID: event.target.value });
-  }
-
+  
   handleSubmit(event) {
     alert("A Film was successfully added: " + this.state.AddTitle);
     event.preventDefault();
@@ -104,19 +98,26 @@ class FilmEntry extends React.Component {
     return (
       <form onSubmit={this.handleSubmit}>
         <label>
+        ID:
+          <input
+            type="text"
+            placeholder="Enter Film ID"
+            value={this.state.AddFilmID}
+            onChange={this.handleChangeFilmID}
+          />
+          Language ID:
+          <input
+            type="text"
+            placeholder="Enter Film Language ID"
+            value={this.state.AddLanguageID}
+            onChange={this.handleChangeLanguageID}
+          />
           Title:
           <input
             type="text"
             placeholder="Enter Film Title"
             value={this.state.AddTitle}
             onChange={this.handleChangeTitle}
-          />
-          ID:
-          <input
-            type="text"
-            placeholder="Enter Film ID"
-            value={this.state.AddFilmID}
-            onChange={this.handleChangeFilmID}
           />
           Description:
           <input
@@ -132,13 +133,7 @@ class FilmEntry extends React.Component {
             value={this.state.AddLength}
             onChange={this.handleChangeLength}
           />
-          Language ID:
-          <input
-            type="text"
-            placeholder="Enter Film Language ID"
-            value={this.state.AddLanguageID}
-            onChange={this.handleChangeLanguageID}
-          />
+          
         </label>
         <input type="submit" value="Add Film" />
       </form>
@@ -157,12 +152,8 @@ class SearchBar extends React.Component {
     this.props.onFilterTextChange(e.target.value);
   }
 
-  // handleSubmit(event) {
-  //   alert("Search Result: " + this.props.filterText);
-  // }
-
   render() {
-    const filterText = this.props.filterText;
+    // const filterText = this.props.filterText;
     return (
       <form
         onSubmit={(e) => {
@@ -182,89 +173,27 @@ class SearchBar extends React.Component {
 }
 
 
-// class ReactPackages extends React.Component {
+// class AddFilm extends React.Component {
 //   constructor(props) {
 //     super(props);
+
 //     this.state = {
-//       movies: [],
-//       totalPackages: null,
+//       postTitle: null,
 //     };
 //   }
 
-
-
-  // render() {
-  //   const rows = [];
-  //   this.state.movies.forEach((reactPackage) => {
-  //     rows.push(<GetFilms reactPackage={reactPackage} />);
-  //   });
-
-  //   return (
-  //     <div>
-  //       <h1>FILM RESULTS</h1>
-  //       <thead style={({ color: "blue" }, { textAlign: "" })}>
-  //         <tr>
-  //           <td>
-  //             <b>Title</b>
-  //           </td>
-  //           <td>
-  //             <b>Film ID</b>
-  //           </td>
-  //           <td>
-  //             <b>Length</b>
-  //           </td>
-  //           <td>
-  //             <b>Description</b>
-  //           </td>
-  //           <td>
-  //             <b>Language ID</b>
-  //           </td>
-  //         </tr>
-  //       </thead>
-  //       <tbody>{rows}</tbody>
-
-  //       <h4>{this.state.totalPackages}</h4>
-  //     </div>
-  //   );
-  // }
-
-
-class AddFilm extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      postTitle: null,
-    };
-  }
-
-  // componentDidMount()
-  render() {
-    const { postfilm } = this.state;
-    return (
-      <div className="card text-center m-3">
-        <h5 className="card-header"></h5>
-        <div className="card-body"></div>
-      </div>
-    );
-  }
-}
-
-// class GetFilms extends React.Component {
+//   // componentDidMount()
 //   render() {
-//     const reactPackage = this.props.reactPackage;
-
+//     const { postfilm } = this.state;
 //     return (
-//       <tr>
-//         <td>{reactPackage.title}</td>
-//         <td>{reactPackage.film_id}</td>
-//         <td>{reactPackage.length}</td>
-//         <td>{reactPackage.description}</td>
-//         <td>{reactPackage.language_id}</td>
-//       </tr>
+//       <div className="card text-center m-3">
+//         <h5 className="card-header"></h5>
+//         <div className="card-body"></div>
+//       </div>
 //     );
 //   }
 // }
+
 
 class FilmDatabase extends React.Component {
   constructor(props) {
@@ -282,7 +211,7 @@ class FilmDatabase extends React.Component {
     fetch("http://3.92.83.234:8080/films")
       .then((response) => response.json())
       .then((jsonData) => {
-        const filminfo = jsonData.slice(0, 100);
+        const filminfo = jsonData.slice(0, 10);
         this.setState({
           movies: filminfo,
           rows: filminfo,
@@ -347,15 +276,11 @@ class FilmDatabase extends React.Component {
             />
           </div>
         </div>
-        {/* <GetFilms
-        movies={this.state.movies}
-        rows={renderRows} /> */}
-        <AddFilm />
+        {/* <AddFilm /> */}
       </div>
     );
   }
 }
-
 
 
 ReactDOM.render(<FilmDatabase />, document.getElementById("root"));
